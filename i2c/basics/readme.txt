bce05403f8dbb3767f1bbe10b6c2559807425b87
ulrich <ubuettemeier@web.de>
2018-09-30 16:47:16 +0200

    function <check_functionality> added

--- i2c/basics/readme.txt
+++ i2c/basics/readme.txt
@@ -7,7 +7,9 @@ script's
 
 // ---------------------------------------------------------------------
 Die Software für I2C Handling ist auf den Raspberry Pi 3 B entwickelt.
-Die Programme arbeiten mit den I2C-Kernelmodulen  i2c_dev und i2c_bcm.
+Die Programme arbeiten mit den I2C-Kernelmodulen i2c_dev und i2c_bcm.
+Es wird der I2C-Bus 1 genutzt. 
+Die ensprechenden Pin's sind SDA.1=Pin 3, SCL.1=Pin 5.
 
 script's
 - source/edit.sh => öffnet die Source Files mit geany
--- i2c/basics/source/test_i2c.c
+++ i2c/basics/source/test_i2c.c
@@ -31,46 +31,81 @@ static void abort_by_error (const char *error_text, const char *func)
   abort();
 }
 /*! --------------------------------------------------------------------
- * 
+ *  @brief  function works like 
+ *            sudo i2cdetect -y 1
  */
 static void scan_i2c (int device) 
 {
-  int i2c_addr;
+    int i2c_addr;
+    int i;
   
-  printf ("scan_i2c\n");
-  for (i2c_addr=0; i2c_addr<128; i2c_addr++) {
-    if (ioctl( device, I2C_SLAVE_FORCE, i2c_addr ) < 0) {
-      abort_by_error ("I2C_SLAVE failed", __func__);
-    } else {
-      if (i2c_smbus_read_byte ( device ) >= 0) printf ("%3X", i2c_addr);  /* chip detected */
-      else printf (" --");
+    if (device < 0) return;
+    printf ("\n   ");
+    for (i=0; i<=0xF; i++) printf ("%3X", i);                           /* print teh low nibble of the i2c_addr */
+    printf ("\n");
+    for (i2c_addr=0; i2c_addr<128; i2c_addr++) {
+        if (((i2c_addr+1) % 16) == 1) printf ("%2X:", (i2c_addr));      /* First print the high nibble of the i2c_addr */
+        
+        if ((i2c_addr > 2) && (i2c_addr < 0x78)) {                      /* scan betwewn 0x02 to 0x78 */
+            if (ioctl( device, I2C_SLAVE_FORCE, i2c_addr ) < 0) {
+                abort_by_error ("I2C_SLAVE failed", __func__);
+            } else {
+                if (i2c_smbus_read_byte ( device ) >= 0) printf ("%3X", i2c_addr);  /* chip detected */
+                else printf (" --");
+            }
+        } else printf ("   ");
+        if (((i2c_addr+1) % 16) == 0) printf ("\n");         /* stdout line feed */
     }
-    if (((i2c_addr+1) % 16) == 0) printf ("\n");  /* stdout line feed */
-  }
-  printf ("\n");
+    printf ("\n");
 }
 /*! --------------------------------------------------------------------
- * 
+ *  @brief  function works like 
+ *               sudo i2cdetect -F 1
+ */
+static unsigned long check_functionality (int device)
+{
+  unsigned long funcs = 0;
+  
+  if (device < 0) 
+      return 0;  
+  if (ioctl( device, I2C_FUNCS, &funcs) < 0)       /* Get the adapter functionality mask */
+      return 0;
+
+  printf ("\n");
+  printf ("%32s = %s\n", "I2C_FUNC_I2C", (funcs & I2C_FUNC_I2C) ? "YES" : "NO");
+  printf ("%32s = %s\n", "I2C_FUNC_10BIT_ADDR", (funcs & I2C_FUNC_10BIT_ADDR) ? "YES" : "NO");
+  printf ("%32s = %s\n", "I2C_FUNC_PROTOCOL_MANGLING", (funcs & I2C_FUNC_PROTOCOL_MANGLING) ? "YES" : "NO");  
+  printf ("%32s = %s\n", "I2C_FUNC_SMBUS_PEC", (funcs & I2C_FUNC_SMBUS_PEC) ? "YES" : "NO");
+  printf ("%32s = %s\n", "I2C_FUNC_SMBUS_BLOCK_PROC_CALL", (funcs & I2C_FUNC_SMBUS_BLOCK_PROC_CALL) ? "YES" : "NO");
+  printf ("%32s = %s\n", "I2C_FUNC_SMBUS_QUICK", (funcs & I2C_FUNC_SMBUS_QUICK) ? "YES" : "NO");
+  printf ("%32s = %s\n", "I2C_FUNC_SMBUS_READ_BYTE", (funcs & I2C_FUNC_SMBUS_READ_BYTE) ? "YES" : "NO");
+  printf ("%32s = %s\n", "I2C_FUNC_SMBUS_WRITE_BYTE", (funcs & I2C_FUNC_SMBUS_WRITE_BYTE) ? "YES" : "NO");
+  printf ("%32s = %s\n", "I2C_FUNC_SMBUS_READ_BYTE_DATA", (funcs & I2C_FUNC_SMBUS_READ_BYTE_DATA) ? "YES" : "NO");
+  printf ("%32s = %s\n", "I2C_FUNC_SMBUS_WRITE_BYTE_DATA", (funcs & I2C_FUNC_SMBUS_WRITE_BYTE_DATA) ? "YES" : "NO");
+  printf ("%32s = %s\n", "I2C_FUNC_SMBUS_READ_WORD_DATA", (funcs & I2C_FUNC_SMBUS_READ_WORD_DATA) ? "YES" : "NO");
+  printf ("%32s = %s\n", "I2C_FUNC_SMBUS_WRITE_WORD_DATA", (funcs & I2C_FUNC_SMBUS_WRITE_WORD_DATA) ? "YES" : "NO");
+  printf ("%32s = %s\n", "I2C_FUNC_SMBUS_PROC_CALL", (funcs & I2C_FUNC_SMBUS_PROC_CALL) ? "YES" : "NO");
+  printf ("%32s = %s\n", "I2C_FUNC_SMBUS_READ_BLOCK_DATA", (funcs & I2C_FUNC_SMBUS_READ_BLOCK_DATA) ? "YES" : "NO");
+  printf ("%32s = %s\n", "I2C_FUNC_SMBUS_WRITE_BLOCK_DATA", (funcs & I2C_FUNC_SMBUS_WRITE_BLOCK_DATA) ? "YES" : "NO");
+  printf ("%32s = %s\n", "I2C_FUNC_SMBUS_READ_I2C_BLOCK", (funcs & I2C_FUNC_SMBUS_READ_I2C_BLOCK) ? "YES" : "NO");
+  printf ("%32s = %s\n", "I2C_FUNC_SMBUS_WRITE_I2C_BLOCK", (funcs & I2C_FUNC_SMBUS_WRITE_I2C_BLOCK) ? "YES" : "NO");
+  
+  return funcs;
+}
+/*! --------------------------------------------------------------------
+ *
  */
 int main(int argc, char *argv[])
 {	  
-  int device;
-  unsigned long funcs;
+  int device;  
   
-  if ((device = open("/dev/i2c-1", O_RDWR)) < 0) {  /* open device file */
+  if ((device = open("/dev/i2c-1", O_RDWR)) < 0)   /* open device file for i2c-bus 1 */
     abort_by_error ("open i2c failed", __func__);
-  }
   
-  if (ioctl( device, I2C_FUNCS, &funcs) < 0) {      /* Get the adapter functionality mask */
-    abort_by_error ("I2C_FUNCS failed", __func__);
-  }
-  
-  /* To determine what functionality is present */
-  if ( funcs & I2C_FUNC_I2C ) printf ("I2c funcs detected\n");
-  if ( funcs & I2C_FUNC_SMBUS_BYTE ) printf ("SMBUS funcs detected\n");
-
-  scan_i2c (device);
+  if (check_functionality (device) == 0) 
+    abort_by_error ("no functionality", __func__);
     
+  scan_i2c (device);    
   close (device);
   
   return EXIT_SUCCESS;
