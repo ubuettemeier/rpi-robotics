91ec167297cff8b69cb6968a59d668c7ef578459
ulrich <ubuettemeier@web.de>
2018-10-13 11:10:43 +0200

    script go.sh new

--- drives/driver_A4988/source/edit.sh
+++ drives/driver_A4988/source/edit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # opens all files with geany
-geany  test_driver_A4988.c Makefile run.sh edit.sh ../readme.txt &
+geany  test_driver_A4988.c Makefile run.sh edit.sh go.sh ../readme.txt &
--- drives/driver_A4988/source/test_driver_A4988.c
+++ drives/driver_A4988/source/test_driver_A4988.c
@@ -9,8 +9,12 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <wiringPi.h>
+#include <sys/time.h>
+#include <pthread.h>
 
 #include "../../../sensor/ultrasonic-HC-SR04/source/keypressed.h"
+
+#define STEP_TIME  750
 
 #define ENABLE_PIN 25     /* GPIO.25 */
 #define STEP_PIN   24     /* GPIO.24 */
@@ -27,6 +31,17 @@
 uint8_t enable = !0;      /* Signal is low active */
 uint8_t dir = 0;
 
+/*!	--------------------------------------------------------------------
+ * @brief   calculates time difference in us
+ * @return  stop - start
+ */
+static int difference_micro (struct timeval *start, struct timeval *stop)
+{
+  return ((signed long long) stop->tv_sec * 1000000ll +
+          (signed long long) stop->tv_usec) -	       
+          ((signed long long) start->tv_sec * 1000000ll +
+           (signed long long) start->tv_usec);
+}
 /*! --------------------------------------------------------------------
  * 
  */
@@ -39,6 +54,8 @@ static void help()
     printf ("2 = toggle dir\n");
     printf ("3 = one step\n");
     printf ("4 = 400 steps\n");
+    printf ("r = run\n");
+    printf ("s = stop run\n");
 }
 /*! --------------------------------------------------------------------
  * 
@@ -47,6 +64,13 @@ int main(int argc, char *argv[])
 {
     char c;
     int taste = 0;
+    struct timeval start, stop;
+    uint8_t run = 0;
+    int timediff;
+    int max_delta = 0;
+    int max_latency = 0;
+    int n = 0;
+    int state = 0;
   
     if( wiringPiSetup() < 0) {
         printf ("wiringPiSetup failed !\n");
@@ -64,29 +88,64 @@ int main(int argc, char *argv[])
     help();
     init_check_keypressed();                           /* init key-touch control */
     
-    while (1) {
-        while ((taste = check_keypressed(&c)) <= 0);  /* wait for keypressed */
-        if (c == 27) break;                           /* quit by ESC */
-        if (c == 'h') help();
-        if (c == '1') {                                /* toggle enable */
-            enable = !enable;
-            digitalWrite (ENABLE_PIN, enable);            
-        }
-        if (c == '2') {                                /* toggle dir */
-            dir = !dir;
-            digitalWrite (DIR_PIN, dir);
-        }
-        if (c == '3') {
-            one_step;            
-        }
-        if (c == '4') {
-            int i;
-            for (i=0; i<400; i++) {                   /* 400 steps = 1 revolution */
+    while (1) {        
+        if ((taste = check_keypressed(&c)) > 0) {          /* look for keypressed */        
+            if (c == 27) break;                           /* quit by ESC */
+            if (c == 'h') help();
+            if (!run) {
+                if (c == '1') {                                /* toggle enable */
+                    enable = !enable;
+                    digitalWrite (ENABLE_PIN, enable);
+                }
+                if (c == '2') {                                /* toggle dir */
+                    dir = !dir;
+                    digitalWrite (DIR_PIN, dir);
+                }
+                if (c == '3') {
+                    one_step;            
+                }
+                if (c == '4') {
+                    int i;
+                    for (i=0; i<400; i++) {                   /* 400 steps = 1 revolution */
+                        one_step;
+                        usleep (1000);                         /* n = 2,5s⁻1 */
+                    }
+                }
+            }
+            
+            if (c == 'r') {
+                enable = !1;
+                digitalWrite (ENABLE_PIN, enable);                
+                run = 1;
+                max_delta = 0;
+                gettimeofday (&start, NULL);
+            }
+            if (c == 's') {
+                run = 0;   
+                printf ("max latency = %i us\n", max_latency);
+            }
+        }   
+        if (run) {
+            gettimeofday (&stop, NULL); 
+            if ((timediff = difference_micro (&start, &stop)) >= STEP_TIME) {                
+                gettimeofday (&start, NULL);                
                 one_step;
-                usleep (1000);                         /* n = 2,5s⁻1 */
+                if (timediff > max_delta) {
+                    max_delta = timediff;
+                    max_latency = timediff - STEP_TIME;
+                }
+                if (n > 600) {
+                    n = 0;
+                    if (state == 0) printf ("/\r");
+                    if (state == 1) printf ("-\r");
+                    if (state == 2) printf ("%c\r", '\\');
+                    fflush(stdout);
+                    state = (state < 2) ? state+1 : 0;
+                }
+                n++;
             }
         }
-        usleep (1000);                                /* wait 1ms */
+        // usleep (1000);                                /* wait 1ms */
     }
     digitalWrite (ENABLE_PIN, (enable = !0));         /* Signal is low aktiv */
     
